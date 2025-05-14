f0de3491b9d103ee856f9c2ab2bd7dcd954761cf
weisanwang <wangweisan1997@163.com>
2025-05-14 17:59:45 +0800

    WIP: shift detector

--- CL_task_agnostic/engine.py
+++ CL_task_agnostic/engine.py
@@ -2,6 +2,8 @@ import torch
 import torchio as tio
 from typing import Iterable
 from torch.utils.tensorboard import SummaryWriter
+from CL_task_agnostic.CL_function import EWCpp
+from CL_task_agnostic.shift_detector import ShiftDetector
 
 def evaluate_batch(model: torch.nn.Module, val_criterion, data_loader: Iterable, device: torch.device, writer, writer_batch: int, args):
     model.eval()
@@ -30,6 +32,10 @@ def train_and_evaluate(model: torch.nn.Module, criterion, val_criterion, data_lo
     loss_window_means=[]
     loss_window_variances=[]
     new_peak_detected=True
+    detector = ShiftDetector(
+        slide_window_length=args.slide_window_length, 
+        mean_threshold=args.mean_threshold, 
+        var_threshold=args.var_threshold)
 
     # Define path of results output dir
     exp_name = "_".join([f"{key}={value}" for key, value in vars(args).items() if key in [
@@ -45,25 +51,48 @@ def train_and_evaluate(model: torch.nn.Module, criterion, val_criterion, data_lo
 
     # Training
     writer_batch = 0
-    for i, task_id in enumerate(args.task_ids):
-        print(f"Training on Task {task_id} =========================================")
-        for j, data in enumerate(data_loader[i]['train']):
+    for i, _ in enumerate(args.task_ids):
+        print(f"Training start =========================================")
+        for _, data in enumerate(data_loader[i]['train']):
+            writer_batch += 1
             # Training
             model.train()
             images = data['mri'][tio.DATA].to(device)
             labels = data['segmentation'][tio.DATA].to(device)
-            optimizer.zero_grad()
             logits = model(images)
+
+            # Basic loss
             loss_batch = criterion(logits, labels)
-            loss_batch.backward()
+            loss_for_peak_detector = loss_batch.item() 
+
+            # Detect if domain shift
+            if detector.update(loss_for_peak_detector):
+                # apply ewc
+                
+
+
+            # Calculate the total loss of current batch
+            if args.cl_method == 'None':
+                total_loss = loss_batch
+            elif args.cl_method == 'ewc':
+                ewc_loss = args.ewc_lambda * ewc.penalty()  
+                writer.add_scalar(f'ewc_loss', ewc_loss, writer_batch)
+                total_loss = criterion(logits, labels) + ewc_loss
+            # elif args.cl_method == 'lwf':
+            #     lwf_loss = args.lwf_lambda * lwf.penalty()
+            #     writer.add_scalar(f'lwf_loss', lwf_loss, writer_batch)
+            #     total_loss = criterion(logits, labels) + lwf_loss
+
+            optimizer.zero_grad()
+            total_loss.backward()
             optimizer.step()
             current_lr = optimizer.param_groups[0]['lr']
 
-            writer_batch += 1
-            print(f"Batch {writer_batch} get loss: {loss_batch}")
+            print(f"Batch {writer_batch} get loss: {total_loss}")
             # print(f"Batch {writer_batch} get learning rate: {current_lr}")
             writer.add_scalar(f'learning rate', current_lr, writer_batch)
-            writer.add_scalar(f'train_loss', loss_batch, writer_batch)
+            writer.add_scalar(f'train_loss', total_loss, writer_batch)
+
 
             # Testing
             evaluate_batch(model, val_criterion, data_loader, device, writer, writer_batch, args)
---
+++ CL_task_agnostic/shift_detector.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+class ShiftDetector:
+    def __init__(self,
+                 slide_window_length: int,
+                 mean_threshold: float,
+                 var_threshold: float):
+        """
+        slide_window_length: The length of slide window
+        mean_threshold     : Threshold for the mean of the loss in the sliding window
+        var_threshold      : Threshold for the variance of the loss in the sliding window
+        """
+        self.slide_window_length = slide_window_length
+        self.mean_threshold = mean_threshold
+        self.var_threshold = var_threshold
+
+        # Keep a sliding window of the last N loss values
+        self.loss_window = []
+
+        # Save the mean and variance of the last two windows
+        self.prev_mean = None
+        self.prev_var  = None
+
+        # State flag: True indicates "Waiting for detecting plateau", False indicates "Wating for detecting new peak"
+        self.new_peak_detected = True
+
+        # Count the number of plateau detected
+        self.plateau_count = 0
+
+    def update(self, loss: float) -> bool:
+        """
+        Called after training each batch:
+        - loss: The loss value (float) of the current batch Returns:
+        - If a domain shift (new peak) is detected, returns True
+        - Otherwise, returns False
+        """
+        # Append the current loss to the sliding window
+        self.loss_window.append(loss)
+        if len(self.loss_window) > self.slide_window_length:
+            del self.loss_window[0]
+
+        # If the sliding window is not full, return False
+        if len(self.loss_window) < self.slide_window_length:
+            return False
+
+        # Calculate the mean and variance of the loss in the sliding window
+        mean = float(np.mean(self.loss_window))
+        var  = float(np.var(self.loss_window))
+
+        # Detect the new peak (domain shift) only after the plateau and not the first window
+        if (not self.new_peak_detected) and (self.prev_mean is not None):
+            # New peak detected if the mean of sliding window increases more than the standard deviation between the two windows
+            if mean - self.prev_mean > np.sqrt(self.prev_var):
+                self.new_peak_detected = True
+                print(
+                    f"[ShiftDetector] 🔺 Domain shift detected!"
+                    f"mean={mean:.4f}, var={var:.6f}"
+                    )
+                # Save the mean and variance of current window
+                self.prev_mean = mean
+                self.prev_var  = var
+                return True
+
+        # Detect the plateau only after the new peak
+        if self.new_peak_detected:
+            if (mean < self.mean_threshold) and (var < self.var_threshold):
+                self.plateau_count += 1
+                print(
+                    f"[ShiftDetector] ── Plateau #{self.plateau_count} detected!"
+                    f"mean={mean:.4f}, var={var:.6f}"
+                )
+                # Switch to "Waiting for next peak" state
+                self.new_peak_detected = False
+
+        # Save the mean and variance of current window
+        self.prev_mean = mean
+        self.prev_var  = var
+
+        return False
